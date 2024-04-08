47159c8f43faeb287031977347109faf23e1f41a
timmka <60577561+gromboy@users.noreply.github.com>
2024-04-09 01:07:41 +0400

    Create 9.2.py

---
+++ 9.2.py
@@ -0,0 +1,12 @@
+def find_numbers_with_4_divisors(start, end):
+    for num in range(start, end + 1):
+        divisors = []
+        for i in range(1, int(num ** 0.5) + 1):
+            if num % i == 0:
+                divisors.append(i)
+                if i != num // i:
+                    divisors.append(num // i)
+        if len(divisors) == 4:
+            print("Число", num, "имеет 4 делителя:", sorted(divisors))
+
+find_numbers_with_4_divisors(258274, 258297)
